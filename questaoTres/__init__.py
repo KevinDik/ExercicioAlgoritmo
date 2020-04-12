0e1bb80c1d863b2dfb9836e9f45a9c420486546a
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2020-04-12 08:55:08 -0300

    continuação da logica do jogo questao 3
    adição da escolha dos participantes e do principio do funcionamento do jogo

--- questaoTres/__init__.py
+++ questaoTres/__init__.py
@@ -1,12 +1,43 @@
-from menu import cabecalho, leiaInt
+from menu import cabecalho, leiaInt, menu
 
 
 def policiaeladrao():
-    from random import shuffle
+    from random import randint
+    policia = []
+    ladrao = []
     jogadores = []
     cabecalho('JOGO Policia e Ladrão')
     partida = leiaInt('Quantos jogadores irão participar? ')
-    for part in range(1, partida+1):
+    for part in range(1, partida + 1):
         jogadores.append(str(input('Digite seu nome: ')))
-    shuffle(jogadores)
-    print(jogadores)
+    while policia == [] and ladrao == []:
+        for pessoa in jogadores:
+            controle = randint(1, 2)
+            if controle == 1:
+                policia.append(pessoa)
+            else:
+                ladrao.append(pessoa)
+    cabecalho('Lista de jogadores')
+    for pos, pessoas in enumerate(policia):
+        print(f'Policia {pos} -------------- {pessoas}')
+    for pos, pessoas in enumerate(ladrao):
+        print(f'Ladrão {pos} -------------- {pessoas}')
+
+    cabecalho('O ladrão se esconde')
+    esconderijo = []
+    proucurar = []
+    for pessoa in ladrao:
+        print(f'{pessoa} ESCOLHA UM LUGAR')
+        esconderijo = menu(['Sala', 'Quarto', 'Garagem', 'Cozinha', 'Sotão', 'Porão', 'Quintal', 'Banheiro'])
+    while True:
+        cabecalho('A Policia proucura')
+        for pessoa in policia:
+            print(f'{pessoa} ESCOLHA UM LUGAR')
+            proucurar = menu(['Sala', 'Quarto', 'Garagem', 'Cozinha', 'Sotão', 'Porão', 'Quintal', 'Banheiro'])
+        if proucurar == esconderijo:
+            print('Ladrão encontrado')
+            break
+
+
+
+
